29a51a24aeb34544644c1eca1f202aa191f82799
quevivasbien <mckaydjensen@gmail.com>
2026-06-07 20:00:44 -0400

    show compiled js in web playground

--- frontend/main.js
+++ frontend/main.js
@@ -1,13 +1,13 @@
-const code = document.getElementById("code");
+const code = document.getElementById('code');
 
 // Set default code text
 const presets = {
-    hello: `func sayHello(name: Str): Str {
+  hello: `func sayHello(name: Str): Str {
     "Hello from " + name + "!"
 };
 
 sayHello("Gema")`,
-    recursiveFactorial: `func factorial(n: Int): Int {
+  recursiveFactorial: `func factorial(n: Int): Int {
     if (n <= 1) {
         1
     } else {
@@ -16,7 +16,7 @@ sayHello("Gema")`,
 };
 
 @map(factorial, range(0, 5))`,
-    iterativeFactorial: `func factorial(n: Int): Int {
+  iterativeFactorial: `func factorial(n: Int): Int {
     if (n < 1) {
         1
     } else {
@@ -28,59 +28,61 @@ sayHello("Gema")`,
     }
 };
 
-@map(factorial, range(0, 5))`,  
+@map(factorial, range(0, 5))`,
 };
 
-code.value = presets[document.getElementById("select-preset").value];
-document.getElementById("select-preset").addEventListener("change", (event) => {
-    code.value = presets[event.target.value];
+code.value = presets[document.getElementById('select-preset').value];
+document.getElementById('select-preset').addEventListener('change', (event) => {
+  code.value = presets[event.target.value];
 });
 
 function insertChars(chars) {
-    const selectionStart = code.selectionStart;
-    const selectionEnd = code.selectionEnd;
-    code.value = code.value.substring(0, selectionStart) + chars + code.value.substring(selectionEnd);
-    code.selectionStart = selectionStart + chars.length;
-    code.selectionEnd = selectionStart + chars.length;
+  const selectionStart = code.selectionStart;
+  const selectionEnd = code.selectionEnd;
+  code.value = code.value.substring(0, selectionStart) + chars + code.value.substring(selectionEnd);
+  code.selectionStart = selectionStart + chars.length;
+  code.selectionEnd = selectionStart + chars.length;
 }
 
 // Capture key presses
-code.addEventListener("keydown", (event) => {
-    if (event.key === "Tab") {
-        event.preventDefault();
-        insertChars("    ");
+code.addEventListener('keydown', (event) => {
+  if (event.key === 'Tab') {
+    event.preventDefault();
+    insertChars('    ');
+  }
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    if (event.ctrlKey) {
+      // If control key is also pressed, run code
+      run();
+    } else {
+      // Otherwise, insert new line, preserving current indent level
+      const currentLine = code.value.slice(0, code.selectionStart).split('\n').pop();
+      const indent = currentLine.match(/^\s*/)[0];
+      insertChars('\n' + indent);
     }
-    if (event.key === "Enter") {
-        event.preventDefault();
-        if (event.ctrlKey) {
-            // If control key is also pressed, run code
-            run();
-        } else {
-            // Otherwise, insert new line, preserving current indent level
-            const currentLine = code.value.slice(0, code.selectionStart).split("\n").pop();
-            const indent = currentLine.match(/^\s*/)[0];
-            insertChars("\n" + indent);
-        }
+  }
+  if (event.key === 'Backspace') {
+    // Special behavior in case of tab before current cursor position
+    if (code.selectionStart === code.selectionEnd && code.value.slice(code.selectionStart - 4, code.selectionStart) === '    ') {
+      const newSstart = code.selectionStart - 4;
+      event.preventDefault();
+      code.value = code.value.slice(0, newSstart) + code.value.slice(code.selectionStart);
+      code.selectionStart = code.selectionEnd = newSstart;
     }
-    if (event.key === "Backspace") {
-        // Special behavior in case of tab before current cursor position
-        if (code.selectionStart === code.selectionEnd && code.value.slice(code.selectionStart - 4, code.selectionStart) === "    ") {
-            const newSstart = code.selectionStart - 4;
-            event.preventDefault();
-            code.value = code.value.slice(0, newSstart) + code.value.slice(code.selectionStart);
-            code.selectionStart = code.selectionEnd = newSstart;
-        }
-    }
+  }
 });
 
 async function run() {
-    const code = document.getElementById("code").value;
-    const response = await fetch("/run", { method: "POST", body: code });
-    document.getElementById("output").innerText = await response.text();
+  const code = document.getElementById('code').value;
+  const response = await fetch('/run', { method: 'POST', body: code });
+  const { js, result } = await response.json();
+  document.getElementById('js-compiled').innerText = js;
+  document.getElementById('output').innerText = result;
 }
 
 // Compile on form submit
-document.getElementById("button-run").addEventListener("click", (event) => {
-    event.preventDefault();
-    run();
+document.getElementById('button-run').addEventListener('click', (event) => {
+  event.preventDefault();
+  run();
 });
--- server.ts
+++ server.ts
@@ -2,13 +2,13 @@ import { serve } from "bun";
 import { join } from "path";
 import { compile } from ".";
 
-function compileAndRun(code: string): string {
+function compileAndRun(code: string): { js: string, result: string } {
     const { errors, js } = compile(code);
     if (errors) {
-        return errors.join("\n");
+        return { js: "", result: errors.join("\n") };
     }
     const result = eval(js);
-    return String(result);
+    return { js, result: String(result) };
 }
 
 serve({
@@ -24,7 +24,7 @@ serve({
         if (request.method === "POST" && url.pathname === "/run") {
             const body = await request.text();
             const result = compileAndRun(body);
-            return new Response(result, { headers: { "content-type": "text/plain" } });
+            return new Response(JSON.stringify(result), { headers: { "content-type": "text/json" } });
         }
 
         return new Response("Not found", { status: 404 });
